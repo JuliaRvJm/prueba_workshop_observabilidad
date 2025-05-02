0f1e1d7054a36187fdf19e308bc6b2d7ba10a42a
julia.rivera@civica-soft.com <julia.rivera@civica-soft.com>
2025-05-02 09:45:20 +0000

    commit change macro schema

--- macros/get_custom_schema.sql
+++ macros/get_custom_schema.sql
@@ -1,4 +1,8 @@
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {{ generate_schema_name_for_env(custom_schema_name, node) }}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema }}
+    {%- else -%}
+        {{ custom_schema_name }}
+    {%- endif -%}
 {%- endmacro %}
